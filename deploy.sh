3502e38ca8292cf4a9ab4063c5409c63e1fab5a8
Shukla <shvshu@8c8590432dbd.ant.amazon.com>
2021-02-27 19:08:38 +0530

    first commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-git clone https://github.com/shivamshukla01/repo2
+git clone https://github.com/shivamshukla01/repo2 --progress --verbose
+pwd
 cd /root/dev/repo2/app
+pwd
+ls
 python3 main.py
